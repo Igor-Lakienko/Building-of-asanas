bf32abe554940cebb95d765692522f496be752ac
family <lakienko.igor@yandex.ru>
2023-01-28 21:11:45 +0300

    Added description my project in file README.md

--- src/main/java/lakienko/com/BuildingAsanas/config/WebSecurityConfig.java
+++ src/main/java/lakienko/com/BuildingAsanas/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .and()
                 .formLogin()
                     .loginPage("/login")
-                    .defaultSuccessUrl("/user")
+                    .defaultSuccessUrl("/")
                     .permitAll()
                     .and()
                     .rememberMe()
